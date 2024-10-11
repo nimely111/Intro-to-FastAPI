252632db4ec750276d645972dc9aa8b7ebd0ffd4
nimely111 <samuelehnimely@gmail.com>
2024-10-11 12:26:59 +0000

    created two files todo.py and models.py file and the complete code for the todos.py file

---
+++ models.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class Todo(BaseModel):
+    id: int
+    item: str
+    completed: bool
+
+    
+class UpdateTodo(BaseModel):
+     id: int
+     item: str
+     completed: Optional[bool] = None
