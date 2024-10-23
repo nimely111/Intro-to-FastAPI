41095505044ab9f7715f8e4f8c4a0b8ca301a8d8
nimely111 <samuelehnimely@gmail.com>
2024-10-23 10:42:41 +0000

    delete stu-record.db file and renamed the same file to stu-post.db in the url string

--- SQLALCHEMY/db/db_setup.py
+++ SQLALCHEMY/db/db_setup.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # database url string
-DATABASE_URL = 'sqlite:///stu-record.db'
+DATABASE_URL = 'sqlite:///stu-post.db'
 # database connection
 engine = create_engine(DATABASE_URL, echo=False)
 # database interaction
