3176daf64b4414582028f582c9250942edba53b3
nimely111 <samuelehnimely@gmail.com>
2024-10-23 10:11:39 +0000

    removed the relationship import from the da seetup file

--- SQLALCHEMY/db/db_setup.py
+++ SQLALCHEMY/db/db_setup.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker
 
 # database url string
 DATABASE_URL = 'sqlite:///stu-record.db'
