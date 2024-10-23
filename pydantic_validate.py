9d30dedd025118f3690622b3316eb799e2762dce
nimely111 <samuelehnimely@gmail.com>
2024-10-23 15:49:29 +0000

    update pydantic file and install email-validator for email validation in pydantic

--- pydantic_validate.py
+++ pydantic_validate.py
@@ -1,1 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+
+# pydantic BENEFITS
+# IDE TYPEHINTS
+# DATA VALIDATION
+# JOSN SERIALIZATION
+
+class User(BaseModel):
+    name: str 
+    email: EmailStr
+    account_id: int
+
+# user = User(name='Sam', email='sam@example.com', account_id=1234)
+# print(user.name)
+user = User(name='Sam', email='sam@example.com', account_id=1234)
+
+print(user.name)
