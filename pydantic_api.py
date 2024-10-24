287937f88684c32d06fa89256ca3ff07e1b3015b
nimely111 <samuelehnimely@gmail.com>
2024-10-24 17:21:49 +0000

    created new file for learning pydantic data validation, type hints, custom validation, serialization

---
+++ pydantic_api.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, status
+from pydantic import BaseModel, Field, field_validator, EmailStr
+from uuid import UUID, uuid4
+
+app = FastAPI()
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str = Field(min_length=5, max_length=12)
+    age: int = Field(gt=13, lt=100)
+
+    
+    class Config:
+        json_schema_extra = {
+            'example':{
+                'email': 'test@example.com',
+                'password': 'test123!',
+                'age': 25,
+            }
+        }
+    
+    @field_validator('password')
+    def password_validator(cls, value):
+        if value == 'test123!':
+            raise ValueError(f"Please do not use default password {value}")
+        return value
+    
+
+
+class User(UserCreate):
+    id: UUID = Field(default_factory=uuid4)
+
+
+
+@app.post('/users/', response_model=User, status_code=status.HTTP_201_CREATED)
+def create_user(user: UserCreate):
+    created_user = User(**user.model_dump())
+    return created_user
